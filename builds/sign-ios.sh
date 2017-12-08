6f81bdfbb8db36f0e322d6ef3d32fc6d5b242f5b
Greg Berns <gregberns@gmail.com>
2017-12-07 17:36:31 -0700

    handle certs better

--- builds/sign-ios.sh
+++ builds/sign-ios.sh
@@ -4,6 +4,17 @@ echo "Start Cert Install"
 
 P12_PASSWORD=$1
 
+
+## ENCRIPT
+# openssl des3 -in ./builds/GregBernsCert.cer -out ./builds/GregBernsCert.cer.enc
+# openssl des3 -in ./builds/GregBernsPem.p12 -out ./builds/GregBernsPem.p12.enc
+# openssl des3 -in ./builds/profile/Greg_Berns_DriveTime_Public_Mobile_Development.mobileprovision -out ./builds/profile/Greg_Berns_DriveTime_Public_Mobile_Development.mobileprovision.enc
+
+# Decript Certs
+openssl des3 -d -in ./builds/GregBernsCert.cer.enc -out ./builds/GregBernsCert.cer -k $P12_PASSWORD
+openssl des3 -d -in ./builds/GregBernsPem.p12.enc -out ./builds/GregBernsPem.p12 -k $P12_PASSWORD
+openssl des3 -d -in ./builds/profile/Greg_Berns_DriveTime_Public_Mobile_Development.mobileprovision.enc -out ./builds/profile/Greg_Berns_DriveTime_Public_Mobile_Development.mobileprovision -k $P12_PASSWORD
+
 #echo $P12_PASSWORD
 
 security create-keychain -p 'tempPassword' ios-build.keychain
